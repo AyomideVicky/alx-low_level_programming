b6c7aceb39da56b66cab6a35fce22ee36e97a26d
ayovicky <ayomidevictoria001@gmail.com>
2023-01-20 18:56:08 -0800

    task3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-wget -P /tmp/ https://raw.github.com/ayovicky/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+#!/bin/bash 
+ wget -P /tmp https://github.com/ayovicky/alx-low_level_programming/raw/main/0x18-dynamic_libraries/iwin.so 
+ export LD_PRELOAD=/tmp/iwin.so
